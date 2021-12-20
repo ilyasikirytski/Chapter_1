3d48cde1a5809d35e93e5207642c3d9303b7f313
Ilya Sikirtytski <ilyasikirytski@gmail.com>
2021-12-21 01:44:29 +0300

    task A_2 - modified interaction through IDE

--- src/A_2.java
+++ src/A_2.java
@@ -1,19 +1,16 @@
 // Отобразить в окне консоли аргументы командной строки в обратном порядке
 
-/*
-Запускал из консоли с помощью:
-C:\Users\Admin\.jdks\openjdk-16.0.2\bin>java -Dfile.encoding=UTF-8 C:\Users\Admin\IdeaProjects\Chapter_1\src\A_2.java hello it's me!
+import java.util.Scanner;
 
-В результате в консоль выводится:
-Аргумент 0 -> hello
-Аргумент 1 -> it's
-Аргумент 2 -> me!
-
- */
 public class A_2 {
     public static void main(String[] args) {
-        for (int i = args.length - 1; i >=0; i--){
-            System.out.printf("Аргумент %s -> %s%n", i, args[i]);
+        System.out.println("Введите аргументы через пробел и нажмите <Enter>:");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        String[] arr = scan.delimiter().split(name);
+        for (int i = arr.length - 1; i >= 0; i--){
+            System.out.printf("Аргумент %s -> %s%n", i, arr[i]);
         }
+        scan.close();
     }
 }
