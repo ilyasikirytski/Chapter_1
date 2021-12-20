ae122beba032863fecd19454cdbfad5ef3566526
Ilya Sikirtytski <ilyasikirytski@gmail.com>
2021-12-20 20:18:25 +0300

    task A_2 - done

---
+++ src/A_2.java
@@ -0,0 +1,19 @@
+// Отобразить в окне консоли аргументы командной строки в обратном порядке
+
+/*
+Запускал из консоли с помощью:
+C:\Users\Admin\.jdks\openjdk-16.0.2\bin>java -Dfile.encoding=UTF-8 C:\Users\Admin\IdeaProjects\Chapter_1\src\A_2.java hello it's me!
+
+В результате в консоль выводится:
+Аргумент 0 -> hello
+Аргумент 1 -> it's
+Аргумент 2 -> me!
+
+ */
+public class A_2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++){
+            System.out.printf("Аргумент %s -> %s%n", i, args[i]);
+        }
+    }
+}
