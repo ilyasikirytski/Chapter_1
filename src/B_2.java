e8e5ba9fc34a7a660815a5015ef595a6b375df90
Ilya Sikirtytski <ilyasikirytski@gmail.com>
2021-12-21 13:10:42 +0300

    task B_1 - fixed

---
+++ src/B_2.java
@@ -0,0 +1,28 @@
+// Ввести с консоли n целых чисел. На консоль вывести: Наибольшее и наименьшее число.
+
+import java.util.Scanner;
+
+public class B_2 {
+    public static void main(String[] args) {
+        System.out.print("Сколько целых чисел вы собираетесь ввести? ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int[] arr = new int[number];
+        System.out.print("Введите целые числа через пробел " + "и нажмите <Enter>: ");
+        for (int i = 0; i < number; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int min = arr[0];
+        for (int i = 0; i < number; i++) {
+            if (min > arr[i])
+                min = arr[i];
+        }
+        System.out.println("Минимльное число:" + min);
+        int max = arr[0];
+        for (int i = 0; i < number; i++) {
+            if (max < arr[i])
+                max = arr[i];
+        }
+        System.out.println("Максимальное число:" + max);
+    }
+}
