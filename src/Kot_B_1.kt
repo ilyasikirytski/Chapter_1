6dbdf96e5ceeb61396c75185d6b1086c7a8b2afd
Ilya Sikirtytski <ilyasikirytski@gmail.com>
2021-12-21 15:34:16 +0300

    task Kot_B_1 - done

---
+++ src/Kot_B_1.kt
@@ -0,0 +1,15 @@
+
+// Ввести с консоли n целых чисел. На консоль вывести: четные и нечетные числа.
+
+fun main() {
+    println("Введите целые числа через пробел и нажмите <Enter>:")
+    val scan = readLine()!!.split(" ").map { it.toInt() }
+    val arr = scan
+    for (i in arr.indices) {
+        if (arr[i] % 2 != 0) {
+            println("Нечётное число:" + arr[i])
+        } else if (arr[i] % 2 == 0) {
+            println("Чётное число:" + arr[i])
+        }
+    }
+}
