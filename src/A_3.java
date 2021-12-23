076751140ba02e30610adaf1c97555952d2c7bc3
Evgeniy Shevtsov <evheniy.shevtsov@gmail.com>
2021-12-23 21:49:01 +0300

    Fix formatting

--- src/A_3.java
+++ src/A_3.java
@@ -2,14 +2,14 @@
 
 public class A_3 {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             double rand = (Math.random() * 20);
-            int result = (int)rand;
+            int result = (int) rand;
             System.out.print(result + " ");
         }
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             double rand = (Math.random() * 20);
-            int result = (int)rand;
+            int result = (int) rand;
             System.out.println(result);
         }
     }
