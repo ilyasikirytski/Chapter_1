9e86d308f6531d2e4f09e76953348d675aa950c0
Evgeniy Shevtsov <evheniy.shevtsov@gmail.com>
2021-12-23 22:25:24 +0300

    Refactor random use and filed names

--- src/A_3.java
+++ src/A_3.java
@@ -1,15 +1,18 @@
 //Вывести заданное количество случайных чисел с переходом и без перехода на новую строку
 
+import java.util.Random;
+
 public class A_3 {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            double rand = (Math.random() * 20);
-            int result = (int) rand;
+        Random random = new Random(System.currentTimeMillis());
+        int numbersCount = 5;
+        int bound = 20;
+        for (int i = 0; i < numbersCount; i++) {
+            int result = random.nextInt(bound);
             System.out.print(result + " ");
         }
-        for (int i = 0; i < 5; i++) {
-            double rand = (Math.random() * 20);
-            int result = (int) rand;
+        for (int i = 0; i < numbersCount; i++) {
+            int result = random.nextInt(bound);
             System.out.println(result);
         }
     }
